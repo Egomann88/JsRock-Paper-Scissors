9a948d015c9b9e55b261eafd2595d629efb570ba
egomann88 <justin-urbanek@bluewin.ch>
2022-06-20 20:49:03 +0200

    Achivement system finished

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,8 @@ let winHtml = "",
   winCounter = 0,
   loseCounter = 0,
   chatMsg = "",
-  endMsg = "Sie haben ";
+  endMsg = "Sie haben ",
+  toggleAchivements = false;
 
 /* init function */
 window.onload = () => {
@@ -24,12 +25,11 @@ window.onload = () => {
   achivementActivaded();  // prüft, welche Sterne aktiviert wurden
 };
 
+/* Achivement-Variabeln */
 let winCounterTotal = localStorage.getItem("WinCounterTotal"),
   loseCountTotal = localStorage.getItem("LoseCounterTotal"),
   WinsInRow = localStorage.getItem("WinsInRow"),
   LosesInRow = localStorage.getItem("LosesInRow");
-
-
 
 /* functionen */
 
@@ -38,6 +38,11 @@ let winCounterTotal = localStorage.getItem("WinCounterTotal"),
  * Vergleicht die Auswahlen und aktualisiert den Counter.<br />
  * Und gibt aus, ob gewonnen oder verloren.<br />
  * @param {number} playerChoice Spieler's auswahl | 0 = Schere; 1 = Stein; 2 = Papier
+ * @func backgroundActivaded potentielle hintergründe für Nutzer freischalten
+ * @func newBackground Meldung für neuen Hintergrund
+ * @func changeStats Achivement-Variablen verändern
+ * @func achivementChecker prüft, ob neuer Erfolg freigestaltet wurde
+ * @func reset spiel wieder zurücksetzen
  */
 function play(playerChoice) {
   // computers wahl
@@ -98,6 +103,7 @@ function play(playerChoice) {
       backgroundActivaded();  // Hintergrund aktivieren
       newBackground();  // neuer Hintergrund Meldung geben
     }
+    changeStats(true);  // permanente Counter umstellen
     setTimeout(() => {
       document.getElementById("modal").classList.toggle("hidden");  // modal schliessen
       document.getElementById("winGif").classList.toggle("hidden"); // Gewinner Gif entfernen
@@ -112,11 +118,30 @@ function play(playerChoice) {
       backgroundActivaded();  // Hintergrund aktivieren
       newBackground();  // neuer Hintergrund Meldung geben
     }
+    changeStats(false);  // permanente Counter umstellen
     setTimeout(() => {
       document.getElementById("modal").classList.toggle("hidden");  // modal schliessen
       document.getElementById("loseGif").classList.toggle("hidden"); // verlierer Gif entfernen
       reset();  // spiel zurücksetzen
     }, 3000);
+  }
+
+  achivementChecker();  // prüft, ob neuer Erfolg freigestaltet wurde
+}
+
+/**
+ * passt die Achivement variablen an
+ * @param {boolean} mode gewonnen oder verloren (gewonnen = true | verloren = false) 
+ */
+function changeStats(mode) {
+  if (mode) { // true
+    localStorage.setItem("winCounterTotal", ++winCounterTotal);
+    localStorage.setItem("WinsInRow", ++WinsInRow);
+    localStorage.setItem("LosesInRow", LosesInRow = 0);
+  } else {  // false
+    localStorage.setItem("loseCountTotal", ++loseCountTotal);
+    localStorage.setItem("LosesInRow", ++LosesInRow);
+    localStorage.setItem("WinsInRow", WinsInRow = 0);
   }
 }
 
@@ -134,10 +159,55 @@ function reset() {
   endHTML.innerText = endMsg;
 }
 
+/**
+ * Geht jedes einzelde Achivent durch und prüft, ob die voraussetzungen erfüllt worden<br></br>
+ * @func newAchivement Wenn ja, wird die meldung gegben
+ * @func achivementActivaded am Ende wird immer geprüft, ob sterne vergoldet werden müssen
+ */
 function achivementChecker() {
-  if (false) {
-
+  if (winCounterTotal >= 5 && localStorage.getItem("Achivement1" == "0")) {
+    localStorage.setItem("Achivement1", "1");
+    newAchivement();
+  } else if (loseCountTotal >= 5 && localStorage.getItem("Achivement2") == "0") {
+    localStorage.setItem("Achivement2", "1");
+    newAchivement();
+  } else if (WinsInRow >= 3 && localStorage.getItem("Achivement3") == "0") {
+    localStorage.setItem("Achivement3", "1");
+    newAchivement();
+  } else if (LosesInRow >= 3 && localStorage.getItem("Achivement4") == "0") {
+    localStorage.setItem("Achivement4", "1");
+    newAchivement();
+  } else if (WinsInRow >= 10 && localStorage.getItem("Achivement5") == "0") {
+    localStorage.setItem("Achivement5", "1");
+    newAchivement();
+  } else if (LosesInRow >= 10 && localStorage.getItem("Achivement6") == "0") {
+    localStorage.setItem("Achivement6", "1");
+    newAchivement();
+  } else if (winCounterTotal >= 100 && localStorage.getItem("Achivement7" == "0")) {
+    localStorage.setItem("Achivement7", "1");
+    newAchivement();
+  } else if (loseCountTotal >= 100 && localStorage.getItem("Achivement8" == "0")) {
+    localStorage.setItem("Achivement8", "1");
+    newAchivement();
   }
+
+  if (  // Psychopath-Achivment
+    localStorage.getItem("Achivement1") == "1" &&
+    localStorage.getItem("Achivement2") == "1" &&
+    localStorage.getItem("Achivement3") == "1" &&
+    localStorage.getItem("Achivement4") == "1" &&
+    localStorage.getItem("Achivement5") == "1" &&
+    localStorage.getItem("Achivement6") == "1" &&
+    localStorage.getItem("Achivement7") == "1" &&
+    localStorage.getItem("Achivement8") == "1" &&
+    localStorage.getItem("Achivement9") == "1" &&
+    localStorage.getItem("Achivement10") == "1"
+  ) {
+    localStorage.setItem("Achivement11", "1");
+    newAchivement();
+  }
+
+  achivementActivaded();  // sterne aktivieren
 }
 
 /**
@@ -163,6 +233,17 @@ async function backgroundActivaded() {
 }
 
 /**
+ * Prüft beim Laden der Seite, welche Achivments freigestaltet wurden und ändert den Stern entsprechet 
+ */
+async function achivementActivaded() {
+  for (let i = 1; i <= 11; i++) { // Hintergrünge jedes Achivements
+    if (localStorage.getItem("Achivement" + i) == "1")
+      document.getElementById("Achivement" + i).classList.add("starAchived"); // aktivierte classe hinzufügen
+    // sonst nichts tun
+  }
+}
+
+/**
  * Golden Wish Achivement
  */
 async function goldenWish() {
@@ -171,17 +252,6 @@ async function goldenWish() {
     location.reload(true);
     newAchivement();
     newBackground();
-  }
-}
-
-/**
- * Prüft beim Laden der Seite, welche Achivments freigestaltet wurden und ändert den Stern entsprechet 
- */
-async function achivementActivaded() {
-  for (let i = 1; i <= 11; i++) { // Hintergrünge jedes Achivements
-    if (localStorage.getItem("Achivement" + i) == "1")
-      document.getElementById("Achivement" + i).classList.add("starAchived"); // aktivierte classe hinzufügen
-    // sonst nichts tun
   }
 }
 
@@ -198,6 +268,15 @@ async function createLocalStorage() {
     if (!localStorage.getItem("Achivement" + i, "0"))  // wenn LS item nicht existiert, dann erstell eins
       localStorage.setItem("Achivement" + i, "0");
   }
+
+  if (!localStorage.getItem("WinCounterTotal"))
+    localStorage.setItem("WinCounterTotal", "0");
+  if (!localStorage.getItem("LoseCounterTotal"))
+    localStorage.setItem("LoseCounterTotal", "0");
+  if (!localStorage.getItem("WinsInRow"))
+    localStorage.setItem("WinsInRow", "0");
+  if (!localStorage.getItem("LosesInRow"))
+    localStorage.setItem("LosesInRow", "0");
 }
 
 /**
@@ -225,4 +304,22 @@ async function newBackground() {
 async function newAchivement() {
   alert('Sie haben eine Erungenschaft erlangen. ( $ _ $ )');
   newBackground();
+}
+
+/**
+ * öffnet den Achivement Contianer
+ */
+async function openAchivements() {
+  toggleAchivements = !toggleAchivements;
+  let time = 0;
+  if (toggleAchivements)
+    time = 0;
+  else
+    time = 900;
+
+  document.getElementById("achivementContainer").classList.toggle("animation-out");
+  document.getElementById("achivementContainer").classList.toggle("animation-in");
+  setTimeout(() => {
+    document.getElementById("achivementContainer").classList.toggle("hidden");
+  }, time);
 }
